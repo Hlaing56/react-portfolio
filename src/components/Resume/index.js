2c1bb59591a8deb6f07dbcb0dc3bd21109275909
Hlaing Aung <betahba@gmail.com>
2022-03-11 10:55:56 -0500

    updated resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,12 +5,12 @@ import coverImage from "../../images/Resume.png";
 function Resume() {
   return (
     <section className="my-5 flex-resume">
-      <img src={coverImage} style={{ width: "65%" }} alt="resume" />
-      <br/>
       <div>
         Download my <a href="https://docs.google.com/document/d/18d1908gCY_3KAt6rk5r_Eps2X-zRg1mYBsivsT3sez0/edit?usp=sharing"
         style={{ textDecoration: "underline" }}> Resume</a>
       </div>
+      <br/>
+      <img src={coverImage} style={{ width: "65%" }} alt="resume" />
     </section>
   );
 }
